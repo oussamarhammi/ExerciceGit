704da51a7cbd79025977b1d13f12a511dcb9e45f
Kevin Pearson Lebreux <6153760@cegepmontpetit.ca>
2022-06-29 13:29:55 -0400

    FCT Classe Booking
    Ajout de la classe Booking

---
+++ S16D_Services/CrazyBooks_Models/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyBooks_Models.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Event { get; set; }
+        public string Commentaire { get; set; }
+    }
+}
