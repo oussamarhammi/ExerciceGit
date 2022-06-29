cfc7d6f7792dfa513834b6961ab4b5113124d725
oussamarhammi <1876469@cegepmontpetit.ca>
2022-06-29 13:35:43 -0400

    fct ajout des champs dans le model

--- S16D_Services/CrazyBooks_Models/Models/CalenderEvent.cs
+++ S16D_Services/CrazyBooks_Models/Models/CalenderEvent.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CrazyBooks_Models.Models
 {
-    class CalenderEvent
+   public class CalenderEvent
     {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Event { get; set; }
+        public string Commentaire { get; set; }
     }
 }
